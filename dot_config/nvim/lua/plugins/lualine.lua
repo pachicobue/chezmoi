2352d2ff7716c4f291d0a88293bf090829512931
pachicobue <tigerssho@gmail.com>
2024-04-15 22:30:24 +0900

    追加

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,43 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "nvim-tree/nvim-web-devicons", opt = true},
+    event = {'BufNewFile', 'BufRead'},
+    options = { theme = 'gruvbox-material' },
+    config = function()
+      local lualine = require('lualine')
+      local config = {
+        options = {
+          component_separators = {},
+          section_separators = {},
+        },
+        sections = {
+          lualine_a = {'filename'},
+          lualine_b = {'branch'},
+          lualine_c = {
+            "'%='",
+            {
+              'diff',
+              symbols = {added = ' ', modified = ' ', removed = ' '},
+              separator = "  |  ",
+            },
+            {
+              'diagnostics',
+              symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
+            },
+          },
+          lualine_x = {'encoding', 'fileformat'},
+          lualine_y = {'filetype','searchcount'},
+          lualine_z = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+      }
+      lualine.setup(config)
+    end
 }
