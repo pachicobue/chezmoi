fae23ac9f952b6c9a8816c37dfd847915e1cd648
pachicobue <tigerssho@gmail.com>
2024-04-30 20:52:35 +0900

    hoge

--- dot_config/nvim/lua/plugins/completion/telescope.lua
+++ dot_config/nvim/lua/plugins/completion/telescope.lua
@@ -15,10 +15,14 @@ return {
         },
         { "nvim-telescope/telescope-ui-select.nvim" },
         { "debugloop/telescope-undo.nvim" },
+        { "nvim-telescope/telescope-file-browser.nvim" },
         {
-            "nvim-telescope/telescope-file-browser.nvim",
-            dependencies = { "nvim-lua/plenary.nvim" },
+            "danielfalk/smart-open.nvim",
+            branch = "0.2.x",
+            dependencies = { "kkharji/sqlite.lua" },
         },
+        { "nvim-telescope/telescope-project.nvim" },
+        { "jvgrootveld/telescope-zoxide" },
         { "nvim-tree/nvim-web-devicons" },
     },
     config = function()
@@ -35,6 +39,28 @@ return {
                         ["n"] = {},
                     },
                 },
+                zoxide = {
+                    prompt_title = "[ Walking on the shoulders of TJ ]",
+                    mappings = {
+                        default = {
+                            after_action = function(selection)
+                                print("Update to (" .. selection.z_score .. ") " .. selection.path)
+                            end,
+                        },
+                        ["<C-s>"] = {
+                            before_action = function(selection)
+                                print("before C-s")
+                            end,
+                            action = function(selection)
+                                vim.cmd.edit(selection.path)
+                            end,
+                        },
+                        -- Opens the selected entry in a new split
+                        ["<C-q>"] = {
+                            action = require("telescope._extensions.zoxide.utils").create_basic_command("split"),
+                        },
+                    },
+                },
             },
         })
         pcall(require("telescope").load_extension, "fzf")
@@ -42,9 +68,32 @@ return {
         pcall(require("telescope").load_extension, "undo")
         pcall(require("telescope").load_extension, "notify")
         pcall(require("telescope").load_extension, "file_browser")
+        pcall(require("telescope").load_extension, "project")
+        pcall(require("telescope").load_extension, "zoxide")
+        pcall(require("telescope").load_extension, "smart_open")
 
         vim.keymap.set("n", "<leader>fn", "<Cmd>Telescope notify<CR>", { desc = "File [N]otify" })
         vim.keymap.set("n", "<leader>fu", "<Cmd>Telescope undo<CR>", { desc = "File [U]ndo" })
-        vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>", { desc = "File [B]rowser" })
+
+        local filebrowser = require("telescope").extensions.file_browser
+        vim.keymap.set("n", "<leader>fb", function()
+            filebrowser.file_browser()
+        end, { desc = "File [B]rowser" })
+
+        local zoxide = require("telescope").extensions.zoxide
+        vim.keymap.set("n", "<leader>fz", zoxide.list, { desc = "File [Z]oxide" })
+
+        local project = require("telescope").extensions.project
+        vim.keymap.set("n", "<leader>fp", function()
+            project.project({})
+        end, { desc = "File [P]roject" })
+
+        local smartopen = require("telescope").extensions.smart_open
+        vim.keymap.set("n", "<leader>ff", function()
+            smartopen.smart_open({ cwd_only = true })
+        end, { desc = "File [F]ind" })
+        vim.keymap.set("n", "<leader>fr", function()
+            smartopen.smart_open({ cwd_only = false })
+        end, { desc = "File [R]ecent" })
     end,
 }
--- dot_config/nvim/lua/plugins/explorer/oil.lua
+++ dot_config/nvim/lua/plugins/explorer/oil.lua
@@ -16,18 +16,12 @@ return {
     },
     opts = {
         keymaps = {
-            -- ["g?"] = "actions.show_help",
             ["<CR>"] = "actions.select",
             ["-"] = "actions.parent",
             ["q"] = "actions.close",
             ["<C-l>"] = "actions.refresh",
             ["<space>"] = "actions.open_cwd",
-            -- ["`"] = "actions.cd",
-            -- ["~"] = "actions.tcd",
             ["g."] = "actions.toggle_hidden",
-            -- ["<C-s>"] = "actions.select_vsplit",
-            -- ["<C-h>"] = "actions.select_split",
-            -- ["<C-t>"] = "actions.select_tab",
         },
         view_options = {
             show_hidden = true,
